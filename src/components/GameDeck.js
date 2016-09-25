27edb74b16fb8a59858d768ed7f650c7055c5497
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-25 14:24:50 -0700

    finish

--- src/components/GameDeck.js
+++ src/components/GameDeck.js
@@ -35,6 +35,28 @@ export default class GameDeck extends Component {
     });
   }
 
+  displayDealerCards(cards) {
+    let CardsImg, cardName;
+    let { scoreEnable } = this.props;
+
+    if(scoreEnable){
+      return cards.map((card, index) => {
+        cardName = card[1];
+        return <span key={ index }><img src={`https://raw.githubusercontent.com/marsholly/Blackjack_Flux/master/CardImgs/${cardName}.png`} width="150" height="200"/>&nbsp;&nbsp;&nbsp;</span>;
+      });
+    } else {
+      let secondCard = cards[1];
+      cardName = secondCard[1];
+      return (
+        <span>
+          <span key='1'><img src='https://raw.githubusercontent.com/marsholly/Blackjack_Flux/master/CardImgs/cardback.png' width="150" height="200"/>&nbsp;&nbsp;&nbsp;</span>
+          <span key='2'><img src={`https://raw.githubusercontent.com/marsholly/Blackjack_Flux/master/CardImgs/${cardName}.png`} width="150" height="200"/>&nbsp;&nbsp;&nbsp;</span>
+        </span>
+      )
+    }
+
+  }
+
   render() {
     let { cards } = this.props;
     let  dealerCardsArr, playerCardsArr, dealerCardsImg, playerCardsImg, dealerScore = 0, playerScore = 0;
@@ -44,7 +66,7 @@ export default class GameDeck extends Component {
       dealerCardsArr = cards.dealerCards;
       playerCardsArr = cards.playerCards;
 
-      dealerCardsImg = this.displayCards(dealerCardsArr);
+      dealerCardsImg = this.displayDealerCards(dealerCardsArr);
       playerCardsImg = this.displayCards(playerCardsArr);
 
       dealerScore = this.scoreHand(dealerCardsArr);
